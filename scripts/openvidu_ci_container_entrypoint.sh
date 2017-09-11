f0ed0aa5a48294e82eaff601409f914952388408
Fede Diaz <nordri@gmail.com>
2017-09-11 16:59:19 +0200

    undoing debbuging

--- scripts/openvidu_ci_container_entrypoint.sh
+++ scripts/openvidu_ci_container_entrypoint.sh
@@ -3,7 +3,7 @@
 echo "##################### EXECUTE: openvidu_ci_container_entrypoint #####################"
 
 [ -n "$1" ] || { echo "No script to run specified. Need one to run after preparing the environment"; exit 1; }
-BUILD_COMMAND=$@
+
 echo mvn $MAVEN_OPTIONS
 for CMD in $BUILD_COMMAND; do
   echo "Running command: $CMD"
