5f111a7bb6e307cf52ac09d0573415e3bb13e010
Fede Diaz <nordri@gmail.com>
2017-09-13 12:06:45 +0200

    changing wget by curl

--- scripts/openvidu_download_release.sh
+++ scripts/openvidu_download_release.sh
@@ -5,6 +5,6 @@ echo "##################### EXECUTE: openvidu_download_release #################
 OV_RELEASE=openvidu-server
 OV_RELEASE_URL=$(curl -s https://api.github.com/repos/openvidu/openvidu/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep $OV_RELEASE)
 
-wget -O openvidu-server.jar $OV_RELEASE_URL
+curl -o openvidu-server.jar $OV_RELEASE_URL
 
 
