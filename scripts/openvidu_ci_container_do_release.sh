0d458f03cee78d73dad71ba0bec57bdc5179537b
Fede Diaz <nordri@gmail.com>
2017-09-22 17:15:18 +0200

    fix type script name

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -24,8 +24,8 @@ case $OPENVIDU_PROJECT in
 
     DESC=$(git log -1 --pretty=%B)
     TAG=$OPENVIDU_VERSION
-    openvidu-github-release.go release --user $OPENVIDU_PROJECT --repo $BASE_NAME --tag $TAG --description $DESC
-    openvidu-github-release.go upload  --user $OPENVIDU_PROJECT --repo $BASE_NAME --tag $TAG --name openvidu-server-${TAG}.jar -f openvidu-server/target/openvidu-server-${TAG}.jar
+    openvidu_github_release.go release --user $OPENVIDU_PROJECT --repo $BASE_NAME --tag $TAG --description $DESC
+    openvidu_github_release.go upload  --user $OPENVIDU_PROJECT --repo $BASE_NAME --tag $TAG --name openvidu-server-${TAG}.jar -f openvidu-server/target/openvidu-server-${TAG}.jar
     ;;
 
   *)
