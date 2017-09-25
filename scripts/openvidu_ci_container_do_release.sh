6658316946fbfbfdcda1da235aa40c4b37a22f92
Fede Diaz <nordri@gmail.com>
2017-09-25 12:06:16 +0200

    path problem

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -38,7 +38,6 @@ sed -i "s/\"version\": \"$PROJECT_VERSION\",/\"version\": \"$OPENVIDU_BROWSER_VE
 npm install
 npm run updatetsc && VERSION=$OPENVIU_BROWSER_VERSION npm run browserify && VERSION=$OPENVIDU_BROWSER_VERSION npm run browserify-prod
 
-cd /opt/openvidu
 openvidu_github_release.go upload --user openvidu --repo $OPENVIDU_REPO --tag "$OPENVIDU_VERSION" --name openvidu-browser-${OPENVIDU_BROWSER_VERSION}.jar -f static/js/openvidu-browser-${OPENVIDU_BROWSER_VERSION}.js
 openvidu_github_release.go upload --user openvidu --repo $OPENVIDU_REPO --tag "$OPENVIDU_VERSION" --name openvidu-browser-${OPENVIDU_BROWSER_VERSION}.jar -f static/js/openvidu-browser-${OPENVIDU_BROWSER_VERSION}.min.js
 
