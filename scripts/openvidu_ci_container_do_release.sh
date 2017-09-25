3e1f994481e1de6896968fd56951ad78c96caaee
Fede Diaz <nordri@gmail.com>
2017-09-25 11:58:34 +0200

    release opevidu browser

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -3,6 +3,7 @@
 echo "##################### EXECUTE: openvidu_ci_container_do_release #####################"
 
 # Verify mandatory parameters
+[ -z "$OPENVIDU_VERSION" ] && exit 1
 [ -z "$OPENVIDU_SERVER_VERSION" ] && exit 1
 [ -z "$OPENVIDU_BROWSER_VERSION" ] && exit 1
 [ -z "$GITHUB_TOKEN" ] && exit 1
@@ -24,19 +25,20 @@ pom-vbump.py -i -v $OPENVIDU_SERVER_VERSION openvidu-server/pom.xml || exit 1
 mvn clean compile package
 
 DESC=$(git log -1 --pretty=%B)
-TAG=$OPENVIDU_SERVER_VERSION
-openvidu_github_release.go release --user openvidu --repo $OPENVIDU_REPO --tag "$TAG" --description "$DESC"
-openvidu_github_release.go upload  --user openvidu --repo $OPENVIDU_REPO --tag "$TAG" --name openvidu-server-${TAG}.jar -f openvidu-server/target/openvidu-server-${TAG}.jar
+openvidu_github_release.go release --user openvidu --repo $OPENVIDU_REPO --tag "$OPENVIDU_VERSION" --description "$DESC"
+openvidu_github_release.go upload  --user openvidu --repo $OPENVIDU_REPO --tag "$OPENVIDU_VERSION" --name openvidu-server-${OPENVIDU_SERVER_VERSION}.jar -f openvidu-server/target/openvidu-server-${OPENVIDU_SERVER_VERSION}.jar
 
 # Openvidu Browser
 cd /opt/$OPENVIDU_REPO
 pom-vbump.py -i -v $OPENVIDU_BROWSER_VERSION openvidu-browser/pom.xml || exit 1
-PROJECT_VERSION=$(grep version openvidu-browser/src/main/resources/package.json | cut -d ":" -f 2 | cut -d "\"" -f 2)
-sed -i "s/\"version\": \"$PROJECT_VERSION\",/\"version\": \"$OPENVIDU_BROWSER_VERSION\",/" openvidu-browser/src/main/resources/package.json
 
-cd /opt/$OPENVIDU_REPO/openvidu-browser
+cd /opt/$OPENVIDU_REPO/openvidu-browser/src/main/resources
+PROJECT_VERSION=$(grep version package.json | cut -d ":" -f 2 | cut -d "\"" -f 2)
+sed -i "s/\"version\": \"$PROJECT_VERSION\",/\"version\": \"$OPENVIDU_BROWSER_VERSION\",/" package.json
+npm install
 npm run updatetsc && VERSION=$OPENVIU_BROWSER_VERSION npm run browserify && VERSION=$OPENVIDU_BROWSER_VERSION npm run browserify-prod
 
-TAG=OPENVIU_BROWSER_VERSION
-openvidu_github_release.go upload  --user openvidu --repo $OPENVIDU_REPO --tag "$TAG" --name openvidu-server-${TAG}.jar -f openvidu-server/target/openvidu-server-${TAG}.jar
+cd /opt/openvidu
+openvidu_github_release.go upload --user openvidu --repo $OPENVIDU_REPO --tag "$OPENVIDU_VERSION" --name openvidu-browser-${OPENVIDU_BROWSER_VERSION}.jar -f static/js/openvidu-browser-${OPENVIDU_BROWSER_VERSION}.js
+openvidu_github_release.go upload --user openvidu --repo $OPENVIDU_REPO --tag "$OPENVIDU_VERSION" --name openvidu-browser-${OPENVIDU_BROWSER_VERSION}.jar -f static/js/openvidu-browser-${OPENVIDU_BROWSER_VERSION}.min.js
 
