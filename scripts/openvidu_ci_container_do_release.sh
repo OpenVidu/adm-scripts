c01e7e57c10c22b43ef3c9dbfa99db4c7a04e98b
Fede Diaz <nordri@gmail.com>
2017-09-25 11:16:25 +0200

    first attemtp to release opevidu browser

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -3,8 +3,8 @@
 echo "##################### EXECUTE: openvidu_ci_container_do_release #####################"
 
 # Verify mandatory parameters
-[ -z "$OPENVIDU_VERSION" ] && exit 1
-[ -z "$BASE_NAME" ] && BASE_NAME=$OPENVIDU_PROJECT
+[ -z "$OPENVIDU_SERVER_VERSION" ] && exit 1
+[ -z "$OPENVIDU_BROWSER_VERSION" ] && exit 1
 [ -z "$GITHUB_TOKEN" ] && exit 1
 
 export PATH=$PATH:$ADM_SCRIPTS
@@ -12,33 +12,31 @@ export PATH=$PATH:$ADM_SCRIPTS
 OPENVIDU_REPO=$(echo $OPENVIDU_GIT_REPOSITORY | cut -d"/" -f2 | cut -d"." -f 1)
 
 git clone $OPENVIDU_GIT_REPOSITORY
-case $OPENVIDU_PROJECT in
 
-  openvidu-server)
-    cd openvidu/openvidu-server/src/angular/frontend
+# Openvidu Server
+cd $OPENVIDU_REPO/openvidu-server/src/angular/frontend
 
-    npm install
-    ng build --output-path ../../main/resources/static
+npm install
+ng build --output-path ../../main/resources/static
 
-    cd /opt/openvidu
-    pom-vbump.py -i -v $OPENVIDU_VERSION openvidu-server/pom.xml || exit 1
-    mvn clean compile package
+cd /opt/openvidu
+pom-vbump.py -i -v $OPENVIDU_SERVER_VERSION openvidu-server/pom.xml || exit 1
+mvn clean compile package
 
-    DESC=$(git log -1 --pretty=%B)
-    TAG=$OPENVIDU_VERSION
-    openvidu_github_release.go release --user openvidu --repo openvidu --tag "$TAG" --description "$DESC"
-    openvidu_github_release.go upload  --user openvidu --repo openvidu --tag "$TAG" --name openvidu-server-${TAG}.jar -f ${PWD}/openvidu-server/target/openvidu-server-${TAG}.jar
-    echo $PWD
-    ls -lh openvidu-server/target
-    ;;
+DESC=$(git log -1 --pretty=%B)
+TAG=$OPENVIDU_SERVER_VERSION
+openvidu_github_release.go release --user openvidu --repo $OPENVIDU_REPO --tag "$TAG" --description "$DESC"
+openvidu_github_release.go upload  --user openvidu --repo $OPENVIDU_REPO --tag "$TAG" --name openvidu-server-${TAG}.jar -f openvidu-server/target/openvidu-server-${TAG}.jar
 
-  *)
-    echo "something went wrong"
-    env
-esac
-    
-    
+# Openvidu Browser
+cd /opt/$OPENVIDU_REPO
+pom-vbump.py -i -v $OPENVIDU_BROWSER_VERSION openvidu-browser/pom.xml || exit 1
+PROJECT_VERSION=$(grep version openvidu-browser/src/main/resources/package.json | cut -d ":" -f 2 | cut -d "\"" -f 2)
+sed -i "s/\"version\": \"$PROJECT_VERSION\",/\"version\": \"$OPENVIDU_BROWSER_VERSION\",/" openvidu-browser/src/main/resources/package.json
 
-    
+cd /opt/$OPENVIDU_REPO/openvidu-browser
+npm run updatetsc && VERSION=$OPENVIU_BROWSER_VERSION npm run browserify && VERSION=$OPENVIDU_BROWSER_VERSION npm run browserify-prod
 
-    
+TAG=OPENVIU_BROWSER_VERSION
+openvidu_github_release.go upload  --user openvidu --repo $OPENVIDU_REPO --tag "$TAG" --name openvidu-server-${TAG}.jar -f openvidu-server/target/openvidu-server-${TAG}.jar
+
