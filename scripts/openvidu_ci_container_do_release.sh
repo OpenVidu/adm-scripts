4698c6bffc5f0c084186ccc1a9fc116df02ac818
Pablo Fuente Pérez <pablofuenteperez@gmail.com>
2018-01-29 13:05:16 +0100

    Update openvidu_ci_container_do_release.sh

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -58,12 +58,13 @@ case $OPENVIDU_PROJECT in
     pushd "$OPENVIDU_PROJECT"
     
     #mvn $MAVEN_OPTIONS versions:set -DnewVersion=${OPENVIDU_VERSION}-SNAPSHOT || (echo "Failed to bump version"; exit 1)
-    pom-vbump.py -i -v ${OPENVIDU_VERSION}-SNAPSHOT pom.xml || (echo "Failed to bump version"; exit 1)
+    #pom-vbump.py -i -v ${OPENVIDU_VERSION}-SNAPSHOT pom.xml || (echo "Failed to bump version"; exit 1)
     #mvn $MAVEN_OPTIONS -DperformRelease=true clean compile package || (echo "Failed to compile"; exit 1)
     #mvn $MAVEN_OPTIONS -DperformRelease=true clean deploy || (echo "Failed to deploy"; exit 1)
-    mvn $MAVEN_OPTIONS release:clean
-    mvn $MAVEN_OPTIONS -DremoteTagging=false release:prepare || (echo "Failed to prepare"; exit 1)
-    mvn $MAVEN_OPTIONS release:perform || (echo "Failed to perform"; exit 1)
+    #mvn $MAVEN_OPTIONS release:clean
+    #mvn $MAVEN_OPTIONS release:update-version || (echo "Failed to prepare"; exit 1)
+    #mvn $MAVEN_OPTIONS release:perform || (echo "Failed to perform"; exit 1)
+    mvn release:clean release:prepare release:perform -DreleaseVersion=${OPENVIDU_VERSION} || (echo "Failed to prepare"; exit 1)
     popd
     ;;
 
