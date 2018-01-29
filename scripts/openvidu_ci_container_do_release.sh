8b5b853c1de5acc4d48255351e394f19b4207552
Pablo Fuente Pérez <pablofuenteperez@gmail.com>
2018-01-29 14:36:01 +0100

    Update openvidu_ci_container_do_release.sh

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -61,7 +61,7 @@ case $OPENVIDU_PROJECT in
     #pom-vbump.py -i -v ${OPENVIDU_VERSION}-SNAPSHOT pom.xml || (echo "Failed to bump version"; exit 1)
     mvn $MAVEN_OPTIONS -DperformRelease=true clean compile package || (echo "Failed to compile"; exit 1)
     mvn $MAVEN_OPTIONS -DperformRelease=true clean deploy || (echo "Failed to deploy"; exit 1)
-    mvn $MAVEN_OPTIONS -DperformRelease=true nexus-staging:promote -DbuildPromotionProfileId=io.openvidu || (echo "Failed to promote"; exit 1)
+    mvn $MAVEN_OPTIONS -DperformRelease=true nexus-staging:promote -DbuildPromotionProfileId=openvidu || (echo "Failed to promote"; exit 1)
     #mvn $MAVEN_OPTIONS release:clean
     #mvn $MAVEN_OPTIONS release:update-version || (echo "Failed to prepare"; exit 1)
     #mvn $MAVEN_OPTIONS release:perform || (echo "Failed to perform"; exit 1)
