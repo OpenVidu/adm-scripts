3160e55c4e120647a3082a1fdba109c35b2e501a
nordri <nordri@gmail.com>
2018-06-06 11:45:37 +0200

    build nightly script

---
+++ scripts/openvidu-build-nightly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+set -eu -o pipefail
+
+# Build nightly version of OpenVidu Server
+# and upload the jar to builds.openvidu.io
+
+MAVEN_OPTIONS='--batch-mode --settings /opt/openvidu-settings.xml -DskipTests=true'
+
+mvn \$MAVEN_OPTIONS compile || exit 1
+mvn \$MAVEN_OPTIONS install || exit 1
+cd openvidu-server
+mvn \$MAVEN_OPTIONS package || exit 1
+
+OV_VERSION=$(get_version_from_pom-xml.py )
+
+ls -lh target/penvidu-server-${OV_VERISON}.jar
