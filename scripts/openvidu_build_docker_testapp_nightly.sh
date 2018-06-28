a6bc38909ed4ebd37829b4ab34e93123725ce4a7
nordri <nordri@gmail.com>
2018-06-28 11:05:29 +0200

    New test app dockerfile and script

--- scripts/openvidu_build_docker_testapp_nightly.sh
+++ scripts/openvidu_build_docker_testapp_nightly.sh
@@ -5,27 +5,33 @@ set -eu -o pipefail
 
 DATESTAMP=$(date +%Y%m%d)
 
-pushd openvidu-testapp/docker
+mkdir -p nginx
+openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout nginx/key.pem -out nginx/cert.pem -days 365
 
-# Download nightly version of OpenVidu Server
-curl -o openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
+cat >nginx/default.conf<<EOF
+server {
+    listen       443;
+    server_name  localhost;
 
-# Download nightly version of TestApp
+    ssl on;
+    ssl_certificate /etc/nginx/conf.d/cert.pem;
+    ssl_certificate_key /etc/nginx/conf.d/key.pem;
+
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+    }
+}
+EOF
+
 mkdir -p web
 curl -O http://builds.openvidu.io/openvidu/nightly/latest/openvidu-testapp-latest.zip
 unzip openvidu-testapp-latest.zip -d web
-
-# Generate SSL
-cd web
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj '/CN=www.mydom.com/O=My Company LTD./C=US' -keyout key.pem -out cert.pem
-openssl pkcs12 -export -in cert.pem -inkey key.pem -out keystore.p12 -password pass:CERT_PASS -name CERT_ALIAS -CAfile cert.pem
-keytool -importkeystore -srckeystore keystore.p12 -srcstoretype PKCS12 -deststorepass CERT_PASS -srcstorepass CERT_PASS -destkeystore NEW.jks -deststoretype JKS
-cd ..
 
 # Build docker image
 docker build --no-cache --rm=true -t openvidu/testapp:nightly-${DATESTAMP} .
 
 # Upload the image
-docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
-docker push openvidu/testapp:nightly-${DATESTAMP} 
-docker logout
+#docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
+#docker push openvidu/testapp:nightly-${DATESTAMP} 
+#docker logout
