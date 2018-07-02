57e7d7b681f4beeff1d832382079306bb652b368
nordri <nordri@gmail.com>
2018-07-02 15:21:48 +0200

    Parametrized environment variable for Openvidu and Openvidu call cases

--- scripts/npm-vbump.py
+++ scripts/npm-vbump.py
@@ -3,12 +3,30 @@
 
 import json
 import os
+import argparse
 
-# Changing version in OpenVidu-browser
-with open('package.json', 'r') as jsonFile:
-	data = json.load(jsonFile)
+def bump_version(ENV_VAR):
+	with open('package.json', 'r') as jsonFile:
+		data = json.load(jsonFile)
 
-data['version'] = os.environ['OPENVIDU_VERSION']
+	data['version'] = os.environ[ENV_VAR]
 
-with open('package.json', 'w') as jsonFile:
-	json.dump(data, jsonFile, sort_keys=True, indent=4)
+	with open('package.json', 'w') as jsonFile:
+		json.dump(data, jsonFile, sort_keys=True, indent=4)
+
+def main():
+
+	parser = argparse.ArgumentParser(
+		description = "We need to now which envvar should pass to the package.json")
+
+	parser.add_argument("--envvar",
+		                metavar = "envvar",
+		                help = "OV_VERSION or OVC_VERSION",
+		                default = "OV_VERSION")
+
+	args = parser.parse_args()
+
+	bump_version(args.envvar)
+
+if __name__ == "__main__":
+	main()
