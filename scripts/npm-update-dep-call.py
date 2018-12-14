f8d85fc80ee20fb24f96edb119352dd627f94e16
Pablo Fuente Pérez <pablofuenteperez@gmail.com>
2018-12-14 15:23:12 +0100

    Update npm-update-dep-call.py

--- scripts/npm-update-dep-call.py
+++ scripts/npm-update-dep-call.py
@@ -5,7 +5,7 @@ import json
 import os
 
 
-# Changing version in openvidu-call frontend
+# Changing versions in openvidu-call frontend
 with open('front/openvidu-call/package.json', 'r') as jsonFile:
     data = json.load(jsonFile)
 
@@ -15,11 +15,12 @@ data['dependencies']['openvidu-browser'] = os.environ['OPENVIDU_CALL_VERSION']
 with open('front/openvidu-call/package.json', 'w') as jsonFile:
     json.dump(data, jsonFile, sort_keys=True, indent=4)
 
-# Changing version in openvidu-angular library
+# Changing versions in openvidu-angular library
 with open('front/openvidu-call/projects/openvidu-angular/package.json', 'r') as jsonFile:
     data = json.load(jsonFile)
 
 data['version'] = os.environ['OPENVIDU_CALL_VERSION']
+data['dependencies']['openvidu-browser'] = os.environ['OPENVIDU_CALL_VERSION']
 
 with open('front/openvidu-call/projects/openvidu-angular/package.json', 'w') as jsonFile:
     json.dump(data, jsonFile, sort_keys=True, indent=4)
