01d24ca5a094278a93f5fd4f557208ace2cf14ef
Fede Diaz <nordri@gmail.com>
2018-12-18 10:31:10 +0100

    Fix typo filename

--- scripts/openvidu-os-update-call.sh
+++ scripts/openvidu-os-update-call.sh
@@ -49,7 +49,7 @@ rm -rf /var/www/html/*
 tar zxf /home/ubuntu/openvidu-call.tar.gz -C /var/www/html
 chown -R www-data.www-data /var/www/html
 rm /home/ubuntu/openvidu-call.tar.gz
-cp ~/OpenViduCall-demos-config-file.json /var/www/html/ov_credentials.json
+cp ~/OpenViduCall-config-file.json /var/www/html/ov_credentials.json
 
 # Starting services
 systemctl start kurento-media-server
