c73ec80f27a5dd2046aeb8ce016f555d06f714b8
Fede Diaz <nordri@gmail.com>
2019-05-07 09:58:41 +0200

    Variable name inside the container is different

--- scripts/openvidu-cloudformation-release.sh
+++ scripts/openvidu-cloudformation-release.sh
@@ -14,15 +14,15 @@ OV_CALL_RELEASE=$(curl --silent "https://api.github.com/repos/openvidu/openvidu-
 [ ! -z "$OVC_VERSION" ] || OVC_VERSION=${OV_CALL_RELEASE}
 
 # CF Version
-sed "s/@CF_V@/${CF_VERSION}/" CF-OpenVidu-TEMPLATE.yaml > CF-OpenVidu-$OV_VERSION.yaml
+sed "s/@CF_V@/${CF_VERSION}/" CF-OpenVidu-TEMPLATE.yaml > CF-OpenVidu-$OPENVIDU_VERSION.yaml
 # OV Version
-sed -i "s/@OV_V@/${OV_VERSION}/" CF-OpenVidu-$OV_VERSION.yaml
+sed -i "s/@OV_V@/${OPENVIDU_VERSION}/" CF-OpenVidu-$OPENVIDU_VERSION.yaml
 # OV Demos Version
-sed -i "s/@OVD_V@/${OVD_VERSION}/" CF-OpenVidu-$OV_VERSION.yaml
+sed -i "s/@OVD_V@/${OVD_VERSION}/" CF-OpenVidu-$OPENVIDU_VERSION.yaml
 # OV Call Version
-sed -i "s/@OVC_V@/${OVC_VERSION}/" CF-OpenVidu-$OV_VERSION.yaml
+sed -i "s/@OVC_V@/${OVC_VERSION}/" CF-OpenVidu-$OPENVIDU_VERSION.yaml
 
-cp CF-OpenVidu-$OV_VERSION.yaml CF-OpenVidu-latest.yaml
+cp CF-OpenVidu-$OPENVIDU_VERSION.yaml CF-OpenVidu-latest.yaml
 
-aws s3 cp CF-OpenVidu-$OV_VERSION.yaml s3://aws.openvidu.io --acl public-read 
+aws s3 cp CF-OpenVidu-$OPENVIDU_VERSION.yaml s3://aws.openvidu.io --acl public-read 
 aws s3 cp CF-OpenVidu-latest.yaml s3://aws.openvidu.io --acl public-read 
