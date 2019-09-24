56c413b25bd9dc27b350524a14a827349d48b034
Fede Diaz <nordri@gmail.com>
2019-09-24 17:10:49 +0200

    debugging

--- scripts/openvidu_nightly_test.sh
+++ scripts/openvidu_nightly_test.sh
@@ -86,6 +86,7 @@ done
 # Testing
 cd openvidu-test-e2e
 cat >run.sh<<EOF
+rgrep version.openvidu.java.client
 mvn --batch-mode -DAPP_URL=https://${TESTAPP_IP}:443/ -DOPENVIDU_URL=https://${OPENVIDU_IP}:4443/ -DREMOTE_URL_CHROME=http://${CHROME_IP}:4444/wd/hub/ -DREMOTE_URL_FIREFOX=http://${FIREFOX_IP}:4444/wd/hub/ test
 echo \$? > res.out
 EOF
