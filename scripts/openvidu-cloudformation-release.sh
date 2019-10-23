0f2ef1dd4d8f5f2c9bea2834772ef64b27268504
Fede Diaz <nordri@gmail.com>
2019-10-23 14:16:55 +0200

    Using extended versioning with v at the beginning

--- scripts/openvidu-cloudformation-release.sh
+++ scripts/openvidu-cloudformation-release.sh
@@ -37,10 +37,8 @@ case $OPENVIDU_PROJECT in
 
     cd cloudformation-openvidu
 
-    CF_VERSION=${OPENVIDU_VERSION}
-    git tag ${CF_VERSION} || exit 1
-    git push --tags
-
+    CF_VERSION=v${OPENVIDU_VERSION}
+    
     TUTORIALS_RELEASE=$(curl --silent "https://api.github.com/repos/openvidu/openvidu-tutorials/releases/latest" | jq --raw-output '.tag_name' | cut -d"v" -f2)
     OV_CALL_RELEASE=$(curl --silent "https://api.github.com/repos/openvidu/openvidu-call/releases/latest" | jq --raw-output '.tag_name' | cut -d"v" -f2)
 
@@ -48,18 +46,21 @@ case $OPENVIDU_PROJECT in
     [ ! -z "$OVC_VERSION" ] || OVC_VERSION=${OV_CALL_RELEASE}
 
     # CF Version
-    sed "s/@CF_V@/${CF_VERSION}/" CF-OpenVidu-TEMPLATE.yaml > CF-OpenVidu-$OPENVIDU_VERSION.yaml
+    sed "s/@CF_V@/${CF_VERSION}/" CF-OpenVidu-TEMPLATE.yaml > CF-OpenVidu-v$OPENVIDU_VERSION.yaml
     # OV Version
-    sed -i "s/@OV_V@/${OPENVIDU_VERSION}/" CF-OpenVidu-$OPENVIDU_VERSION.yaml
+    sed -i "s/@OV_V@/${OPENVIDU_VERSION}/" CF-OpenVidu-v$OPENVIDU_VERSION.yaml
     # OV Demos Version
-    sed -i "s/@OVD_V@/${OVD_VERSION}/" CF-OpenVidu-$OPENVIDU_VERSION.yaml
+    sed -i "s/@OVD_V@/${OVD_VERSION}/" CF-OpenVidu-v$OPENVIDU_VERSION.yaml
     # OV Call Version
-    sed -i "s/@OVC_V@/${OVC_VERSION}/" CF-OpenVidu-$OPENVIDU_VERSION.yaml
+    sed -i "s/@OVC_V@/${OVC_VERSION}/" CF-OpenVidu-v$OPENVIDU_VERSION.yaml
 
-    cp CF-OpenVidu-$OPENVIDU_VERSION.yaml CF-OpenVidu-latest.yaml
+    cp CF-OpenVidu-v$OPENVIDU_VERSION.yaml CF-OpenVidu-latest.yaml
 
-    aws s3 cp CF-OpenVidu-$OPENVIDU_VERSION.yaml s3://aws.openvidu.io --acl public-read 
+    aws s3 cp CF-OpenVidu-v$OPENVIDU_VERSION.yaml s3://aws.openvidu.io --acl public-read 
     aws s3 cp CF-OpenVidu-latest.yaml s3://aws.openvidu.io --acl public-read 
+
+    git tag ${CF_VERSION} || exit 1
+    git push --tags
 
     ;;
 
