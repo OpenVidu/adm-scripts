6d2f5a2ed325e5dea26e01f1c325332abfb5737e
Fede Diaz <nordri@gmail.com>
2019-11-12 14:21:26 +0100

    Script to update DynamoDB table kms_ov_matrix

--- scripts/openvidu_update_kms_ov_matrix.sh
+++ scripts/openvidu_update_kms_ov_matrix.sh
@@ -6,34 +6,6 @@ set -x
 
 export AWS_DEFAULT_REGION=eu-west-1
 
-while [[ $# -gt 0 ]]; do
-    case "${1-}" in
-        --openvidu-version)
-        	if [[ -n "${2-}" ]]; then
-                export OV_VERSION="$2"
-                shift
-            else
-                echo "ERROR: --openvidu-version expects <OpenViduVersion>"
-                exit 1
-            fi
-            ;;
-        --kurento-version)
-        	if [[ -n "${2-}" ]]; then
-                export KMS_VERSION="$2"
-                shift
-            else
-                echo "ERROR: --kurento-version expects <KurentoVersion>"
-                exit 1
-            fi
-            ;;
-        *)
-			echo "ERROR: Unknown argument '${1-}'"
-            exit 1
-            ;;
-    esac
-    shift
-done
-
 aws dynamodb put-item \
   --table-name ov_kms \
   --item "{
