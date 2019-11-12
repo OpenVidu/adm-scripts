0d379f1ee32f774cd92365eb8cbaf5d0087da90d
Fede Diaz <nordri@gmail.com>
2019-11-12 15:55:07 +0100

    Improve readability

--- scripts/openvidu_update_kms_ov_matrix.sh
+++ scripts/openvidu_update_kms_ov_matrix.sh
@@ -8,8 +8,8 @@ export AWS_DEFAULT_REGION=eu-west-1
 
 aws dynamodb put-item \
   --table-name ov_kms \
-  --item "{
-    \"ov\":  {\"S\": \"${OPENVIDU_VERSION}\"},
-    \"kms\": {\"S\": \"${KMS_VERSION}\"} 
-    }"
+  --item '{
+    "ov":  {"S": '\"${OPENVIDU_VERSION}\"'},
+    "kms": {"S": '\"${KMS_VERSION}\"'} 
+    }'
 
