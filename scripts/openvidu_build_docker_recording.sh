4ac84a92271d2c05008b64bbcbd8d060e8ef1b95
cruizba <charlesrb94@outlook.com>
2020-07-01 20:52:56 +0200

    Added script to build openvidu recording image

---
+++ scripts/openvidu_build_docker_recording.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+OPENVIDU_RECORDING_UBUNTU_VERSION=$1
+OPENVIDU_RECORDING_CHROME_VERSION=$2
+OPENVIDU_RECORDING_DOCKER_TAG=$3
+docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
+docker build --no-cache --rm=true --build-arg CHROME_VERSION="$OPENVIDU_RECORDING_CHROME_VERSION" \
+    -f $OPENVIDU_RECORDING_UBUNTU_VERSION.Dockerfile \
+    -t openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG . \
+    && docker push openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG
+docker logout
