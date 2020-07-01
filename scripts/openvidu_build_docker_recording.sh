fad30cdaad83b36addda344e87fff83b90231541
cruizba <charlesrb94@outlook.com>
2020-07-01 20:56:40 +0200

    Use env variables as parameters only in openvidu_build_docker_recording.sh

--- scripts/openvidu_build_docker_recording.sh
+++ scripts/openvidu_build_docker_recording.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
-OPENVIDU_RECORDING_UBUNTU_VERSION=$1
-OPENVIDU_RECORDING_CHROME_VERSION=$2
-OPENVIDU_RECORDING_DOCKER_TAG=$3
+
+### Variables ###
+OPENVIDU_RECORDING_UBUNTU_VERSION=$OPENVIDU_RECORDING_UBUNTU_VERSION
+OPENVIDU_RECORDING_CHROME_VERSION=$OPENVIDU_RECORDING_CHROME_VERSION
+OPENVIDU_RECORDING_DOCKER_TAG=$OPENVIDU_RECORDING_DOCKER_TAG
+
+# Change directory to openvidu-recording
+pushd openvidu-server/docker/openvidu-recording
+
+# Docker login
 docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
+
+# Build image with parameters
 docker build --no-cache --rm=true --build-arg CHROME_VERSION="$OPENVIDU_RECORDING_CHROME_VERSION" \
     -f $OPENVIDU_RECORDING_UBUNTU_VERSION.Dockerfile \
     -t openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG . \
     && docker push openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG
+
 docker logout
