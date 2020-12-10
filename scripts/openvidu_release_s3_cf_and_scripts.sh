fea46b0c89adcbc9c1615b6b6e30df5c3da2c8e0
cruizba <charlesrb94@outlook.com>
2020-12-10 17:04:57 +0100

    openvidu_release_s3_cf_and_scripts.sh: Typo scripts sh extension

--- scripts/openvidu_release_s3_cf_and_scripts.sh
+++ scripts/openvidu_release_s3_cf_and_scripts.sh
@@ -14,6 +14,6 @@ aws s3 cp s3://aws.openvidu.io/CF-OpenVidu-"${FROM_VERSION}".yaml s3://aws.openv
 aws s3 cp s3://aws.openvidu.io/CF-OpenVidu-Pro-"${FROM_VERSION}".yaml s3://aws.openvidu.io/CF-OpenVidu-Pro-"${TO_VERSION}".yaml --acl public-read
 
 echo "Updating installation scripts from ${FROM_VERSION} to ${TO_VERSION}"
-aws s3 cp s3://aws.openvidu.io/install_openvidu_"${FROM_VERSION}".yaml s3://aws.openvidu.io/install_openvidu_"${TO_VERSION}".yaml --acl public-read
-aws s3 cp s3://aws.openvidu.io/install_openvidu_pro_"${FROM_VERSION}".yaml s3://aws.openvidu.io/install_openvidu_pro_"${TO_VERSION}".yaml --acl public-read
-aws s3 cp s3://aws.openvidu.io/install_media_node_"${FROM_VERSION}".yaml s3://aws.openvidu.io/install_media_node_"${TO_VERSION}".yaml --acl public-read
+aws s3 cp s3://aws.openvidu.io/install_openvidu_"${FROM_VERSION}".sh s3://aws.openvidu.io/install_openvidu_"${TO_VERSION}".sh --acl public-read
+aws s3 cp s3://aws.openvidu.io/install_openvidu_pro_"${FROM_VERSION}".sh s3://aws.openvidu.io/install_openvidu_pro_"${TO_VERSION}".sh --acl public-read
+aws s3 cp s3://aws.openvidu.io/install_media_node_"${FROM_VERSION}".sh s3://aws.openvidu.io/install_media_node_"${TO_VERSION}".sh --acl public-read
