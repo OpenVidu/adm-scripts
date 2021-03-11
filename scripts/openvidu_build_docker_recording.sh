53dd739ea2b5f2c45b605cebc873a3ccde9e2dfe
pabloFuente <pablofuenteperez@gmail.com>
2021-03-11 13:48:17 +0100

    Updated recording image build script

--- scripts/openvidu_build_docker_recording.sh
+++ scripts/openvidu_build_docker_recording.sh
@@ -11,14 +11,8 @@ pushd openvidu-server/docker/openvidu-recording
 # Docker login
 docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
 
-if [[ "$OPENVIDU_RECORDING_UBUNTU_VERSION" == "ubuntu-20-04" ]]; then
-    docker pull ubuntu:20.04
-elif [[ "$OPENVIDU_RECORDING_UBUNTU_VERSION" == "ubuntu-16-04" ]]; then
-    docker pull ubuntu:16.04
-fi
-
 # Build image with parameters
-docker build --no-cache --rm=true --build-arg CHROME_VERSION="$OPENVIDU_RECORDING_CHROME_VERSION" \
+docker build --pull --no-cache --rm=true --build-arg CHROME_VERSION="$OPENVIDU_RECORDING_CHROME_VERSION" \
     -f $OPENVIDU_RECORDING_UBUNTU_VERSION.Dockerfile \
     -t openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG . \
     && docker push openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG
