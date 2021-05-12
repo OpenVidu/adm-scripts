e1e5fe2058e14dcfd9d3ba145af784956e42a3b4
cruizba <charlesrb94@outlook.com>
2021-05-12 11:51:54 +0200

    Create openvidu CE betas

--- scripts/openvidu_build_docker_server_nightly.sh
+++ scripts/openvidu_build_docker_server_nightly.sh
@@ -10,9 +10,18 @@ pushd Docker/openvidu-server-nightly
 curl -o openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
 
 # Build docker image
-docker build --pull --no-cache --rm=true -t openvidu/openvidu-server:nightly-${DATESTAMP} .
+if [[ -z "${OV_VERSION}" ]]; then
+    docker build --pull --no-cache --rm=true -t openvidu/openvidu-server:nightly-"${DATESTAMP}" .
+else 
+    docker build --pull --no-cache --rm=true -t openvidu/openvidu-server:"${OV_VERSION}" .
+fi
 
 # Upload the image
 docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
-docker push openvidu/openvidu-server:nightly-${DATESTAMP} 
+if [[ -z "${OV_VERSION}" ]]; then
+    docker push openvidu/openvidu-server:nightly-"${DATESTAMP}"
+else 
+    docker push openvidu/openvidu-server:"${OV_VERSION}"
+fi
+
 docker logout
