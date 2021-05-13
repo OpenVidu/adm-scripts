13bd18eb4e2c00bbd40e8ad852513f72b9657d68
cruizba <charlesrb94@outlook.com>
2021-05-13 21:51:09 +0200

    OpenVidu call docker release build script

---
+++ scripts/openvidu_call_docker_release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -xe
+set -eu -o pipefail
+
+echo "##################### EXECUTE: openvidu_ci_container_build #####################"
+
+[ -n "${OV_VERSION}" ] || exit 1
+[ -n "${RELEASE}" ] || RELEASE='false'
+[ -n "${OPENVIDU_CALL_BRANCH}" ] || OPENVIDU_CALL_BRANCH='master'
+[ -n "${OPENVIDU_BROWSER_BRANCH}" ] || OPENVIDU_BROWSER_BRANCH='master' 
+
+if [ "${OPENVIDU_CALL_BRANCH}" != 'master' ]; then
+    git checkout "${OPENVIDU_CALL_BRANCH}"
+fi
+
+# Login to dockerhub
+docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
+
+if [[ "${RELEASE}" == 'true' ]]; then
+    pushd docker
+    chmod u+x run.sh
+    ./run.sh "${OV_VERSION}" "${OPENVIDU_CALL_BRANCH}"
+    popd
+else
+    docker build -f docker/custom.dockerfile -t openvidu/openvidu-call:"${OV_VERSION}" --build-arg OPENVIDU_BROWSER="${OPENVIDU_BROWSER_BRANCH}" . || exit 1
+    docker push openvidu/openvidu-call:"${OV_VERSION}"
+fi
+
+docker logout
+
+# Remove dangling images
+if [ $(docker images -f "dangling=true" -q | wc -l) -ne 0 ]; then
+  docker rmi $(docker images -f "dangling=true" -q) || exit 0
+fi
