811f711f6cc2bf4d8e04102e08bc40f30be3d938
cruizba <carlos.ruizbal@gmail.com>
2021-09-29 15:41:58 +0200

    Use script to only check openvidu-server image

--- scripts/check_openvidu_release_docker.sh
+++ scripts/check_openvidu_release_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 [ -n "${OPENVIDU_DOCKERHUB_USER}" ] || exit 1
 [ -n "${OPENVIDU_DOCKERHUB_PASSWD}" ] || exit 1
-[ -n "${DOCKER_IMAGE}" ] || exit 1
+[ -n "${OV_VERSION}" ] || exit 1
 
 
 # If version exist in DockerHub, stop execution. Nightly builds must have the current version in development
 docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD" &> /dev/null
 
-docker manifest inspect "${DOCKER_IMAGE}" > /dev/null ;
+docker manifest inspect openvidu/openvidu-server:"${OV_VERSION}" > /dev/null ;
 EXIST=$?
 if [[ "${EXIST}" -eq 0 ]]; then
     echo "true"
