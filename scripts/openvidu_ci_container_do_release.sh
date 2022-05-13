9143dd44d2d8e8af0656670817787e8a4e269c2b
cruizba <carlos.ruizbal@gmail.com>
2022-05-13 15:49:16 +0200

    Try fix call-react release...

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -279,6 +279,8 @@ case $OPENVIDU_PROJECT in
 
     # Build openvidu-react library
     cd ../openvidu-call-react
+    npm cache clean --force
+    sudo chown -R 1001:1001 "/root/.npm"
     npm run build:openvidu-react || { echo "Failed to build openvidu-react library"; exit 1; }
 
     # Publish openvidu-react library
