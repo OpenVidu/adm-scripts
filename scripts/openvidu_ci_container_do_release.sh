965aa567a45aa9a80608419c3683885583595f2c
cruizba <carlos.ruizbal@gmail.com>
2022-09-21 18:21:43 +0200

    Temporary sleep to debug

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -62,6 +62,7 @@ case $OPENVIDU_PROJECT in
     export OPENVIDU_CALL_VERSION="${OPENVIDU_VERSION}"
     npm-update-dep-ov-components-angular.py || { echo "Faile to update dependencies/bump version"; exit 1; }
 
+    sleep infinity
     npm run lib:build || { echo "Failed to 'npm run lib:build'"; exit 1; }
     pushd dist/openvidu-angular
     npm publish || { echo "Failed to publish openvidu-angular to npm"; exit 1; }
