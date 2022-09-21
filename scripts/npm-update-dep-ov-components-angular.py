9a0936905aa5ad194fcfc424d61a9ce2be84897a
cruizba <carlos.ruizbal@gmail.com>
2022-09-21 18:39:47 +0200

    Fix update version openvidu-components

--- scripts/npm-update-dep-ov-components-angular.py
+++ scripts/npm-update-dep-ov-components-angular.py
@@ -21,7 +21,7 @@ with open('projects/openvidu-angular/package.json', 'r') as jsonFile:
     data = json.load(jsonFile, object_pairs_hook=OrderedDict)
 
 data['version'] = os.environ['OPENVIDU_CALL_VERSION']
-data['dependencies']['openvidu-browser'] = os.environ['OPENVIDU_CALL_VERSION']
+data['peerDependencies']['openvidu-browser'] = os.environ['OPENVIDU_CALL_VERSION']
 
 with open('projects/openvidu-angular/package.json', 'w') as jsonFile:
     json.dump(data, jsonFile, sort_keys=True, indent=4)
