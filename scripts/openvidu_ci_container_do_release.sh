79287e1bcfa0bc38d8ee1e32b9dceb778b105b67
cruizba <carlos.ruizbal@gmail.com>
2023-01-10 12:09:53 +0100

    Remove temporal sleep to check maven configuration

--- scripts/openvidu_ci_container_do_release.sh
+++ scripts/openvidu_ci_container_do_release.sh
@@ -154,10 +154,6 @@ case $OPENVIDU_PROJECT in
     ;;
   openvidu-java-client)
 
-    echo "Sleeping..."
-    sleep infinity
-    exit 1
-
     echo "## Building openvidu-java-client"
     [ -z "$OPENVIDU_VERSION" ] && { echo "OPENVIDU_VERSION is empty"; exit 1; }
 
