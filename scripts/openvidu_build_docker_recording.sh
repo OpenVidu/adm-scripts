828187724e3f7f8012192c5a638463b365dc92c2
pabloFuente <pablofuenteperez@gmail.com>
2023-02-27 11:44:05 +0100

    Update openvidu-recording build script

--- scripts/openvidu_build_docker_recording.sh
+++ scripts/openvidu_build_docker_recording.sh
@@ -1,20 +1,15 @@
 #!/bin/bash -x
 
-### Variables ###
-OPENVIDU_RECORDING_UBUNTU_VERSION=$OPENVIDU_RECORDING_UBUNTU_VERSION
-OPENVIDU_RECORDING_CHROME_VERSION=$OPENVIDU_RECORDING_CHROME_VERSION
-OPENVIDU_RECORDING_DOCKER_TAG=$OPENVIDU_RECORDING_DOCKER_TAG
-
 # Change directory to openvidu-recording
 pushd openvidu-server/docker/openvidu-recording
 
 # Docker login
-docker login -u "$OPENVIDU_DOCKERHUB_USER" -p "$OPENVIDU_DOCKERHUB_PASSWD"
+docker login -u $OPENVIDU_DOCKERHUB_USER -p $OPENVIDU_DOCKERHUB_PASSWD
 
 # Build image with parameters
-docker build --pull --no-cache --rm=true --build-arg CHROME_VERSION="$OPENVIDU_RECORDING_CHROME_VERSION" \
-    -f $OPENVIDU_RECORDING_UBUNTU_VERSION.Dockerfile \
-    -t openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG . \
-    && docker push openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG
+./create_image.sh $OPENVIDU_RECORDING_CHROME_VERSION $OPENVIDU_RECORDING_DOCKER_TAG
+
+# Push image
+docker push openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG
 
 docker logout
